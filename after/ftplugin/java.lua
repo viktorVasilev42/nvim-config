3ec8b80530f95f0840968a90d30361ddb56951c3
viktorVasilev42 <vasilevviktor03@gmail.com>
2024-06-09 10:46:00 +0200

    inital

---
+++ after/ftplugin/java.lua
@@ -0,0 +1,55 @@
+-- Java Language Server configuration.
+-- Locations:
+-- 'nvim/ftplugin/java.lua'.
+-- 'nvim/lang-servers/intellij-java-google-style.xml'
+
+local jdtls_ok, jdtls = pcall(require, "jdtls")
+if not jdtls_ok then
+  vim.notify "JDTLS not found, install with `:LspInstall jdtls`"
+  return
+end
+
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls"
+local path_to_lsp_server = jdtls_path .. "/config_win/"
+local path_to_plugins = jdtls_path .. "/plugins/"
+local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.800.v20240330-1250.jar"
+local path_to_lombok = jdtls_path .. "/lombok.jar"
+
+local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+if root_dir == "" then
+  return
+end
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
+os.execute("mkdir " .. workspace_dir)
+
+-- Main Config
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-javaagent:' .. path_to_lombok,
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar', path_to_jar,
+    '-configuration', path_to_lsp_server,
+    '-data', workspace_dir,
+  },
+}
+
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
